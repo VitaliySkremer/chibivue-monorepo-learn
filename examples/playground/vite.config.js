d59decd75109f1996154e050296f0c3cdfd90e69
Vitalij Syomkin <syomkin@1221systems.ru>
2026-04-03 10:52:40 +0300

    настройка GH pages

--- examples/playground/vite.config.js
+++ examples/playground/vite.config.js
@@ -3,4 +3,13 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
-export default defineConfig({});
+export default defineConfig({
+  base: (() => {
+    const raw = process.env.VITE_BASE_URL;
+    const rawTrimmed = raw?.trim();
+
+    if (!rawTrimmed) return "/";
+
+    return rawTrimmed;
+  })()
+});
